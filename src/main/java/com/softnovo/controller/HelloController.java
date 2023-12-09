6f0e271e29e5c252d60e7a9f29aa81eaf62265cb
cgm <plextocao@gmail.com>
2023-12-09 22:38:32 +0800

    hutools

---
+++ src/main/java/com/softnovo/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.softnovo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/app")
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/{accountId}")
+    public String getAccountById(@PathVariable("accountId") Long accountId) {
+        return "account __" + accountId;
+    }
+}
